ae2ba4998ebc0aaf15c8b6691f28e9452cb24a2a
mohamad <aghi.mohamad@gmail.com>
2024-07-31 00:29:11 +0300

    dockerizing the app

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/note-taking-CRUD-filtering-tags-with-CI-CD/', // Update this to match your repository name
   plugins: [react()],
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+   },
   test: {
     globals : true,
     environment: 'jsdom',
